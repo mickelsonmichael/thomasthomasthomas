496888f1ac9d7e61f4bc551d958574216f279d8f
Michael Mickelson <mickelsonmichael@protonmail.com>
2025-11-02 15:32:05 +0000

    feat: init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# thomasthomasthomas
+# Thomas Watch This
+
+Simply make a commit to add a movies to `movies.json`.
+The first movie in the list will be displayed on the site.
---
+++ main.js
@@ -0,0 +1,35 @@
+const MOVIES_FILE_NAME = "movies.json"
+
+function printError(error) {
+    const ERROR_CLASS = "error";
+
+    console.error(error);
+
+    const errorElement = document.querySelector(`.${ERROR_CLASS}`);
+
+    errorElement.innerHTML = error;
+    errorElement.classList.add("show");
+}
+
+function renderMovies(movies) {
+    const CONTAINER_CLASS = "movie";
+    const LOADER_CLASS = "loader";
+
+    const movieElement = document.querySelector(`.${CONTAINER_CLASS}`);
+    const loaderElement = document.querySelector(`.${LOADER_CLASS}`);
+
+    const movie = movies[0];
+
+    movieElement.innerHTML = movie.title;
+    loaderElement.classList.remove("show");
+    movieElement.classList.add("show");
+}
+
+function loadMovies() {
+    fetch(`/${MOVIES_FILE_NAME}`)
+        .then(res => res.json())
+        .then(renderMovies)
+        .catch(printError);
+}
+
+loadMovies();
